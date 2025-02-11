20eb30c02b03fa6a6b1ba67ecec3bea07ea5b4ef
Dikeshkhatree <dikeshkhatree@gmail.com>
2025-02-11 14:35:25 +0545

    used resource class till now

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::resource('users', UserController::class);
+Route::resource('roles', RoleController::class);
+
