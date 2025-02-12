fe53625b9f1310b71697d0659957aeed3c4a7556
Dikeshkhatree <dikeshkhatree@gmail.com>
2025-02-12 17:26:10 +0545

    reln betn role and permis

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Role;
+
+use App\Models\Permission;
+use Illuminate\Http\Request;
+
+class PermissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // $permission = Permission::find(5);
+        // return $permission->roles;
+         $permissions = Permission::all();
+
+        return view('permissions.index', compact('permissions'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'display_name' => 'required',
+        ]);
+
+        $role = Role::create([
+            'name' => $request->name,
+            'display_name' => $request->display_name
+        ]);
+
+        $permissions = $request->input('permissions');//from form
+
+        $role->permissions()->sync($permissions);
+
+        return redirect()->route('permissions.index')->with('success', 'ok');
+    }
+    
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
