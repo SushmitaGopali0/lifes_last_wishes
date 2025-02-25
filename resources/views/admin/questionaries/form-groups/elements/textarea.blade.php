f4effc754c0b7d9f4ab6884a008afca4981a7bd0
Dikeshkhatree <dikeshkhatree@gmail.com>
2025-02-25 17:24:54 +0545

    added textarea in elements

---
+++ resources/views/admin/questionaries/form-groups/elements/textarea.blade.php
@@ -0,0 +1,4 @@
+<div class="mb-3">
+    <label for="prefilled_text">Pre-filled Text:</label>
+    <textarea name="prefilled_text" id="prefilled_textarea" class="form-control" rows="3"></textarea>
+</div>
